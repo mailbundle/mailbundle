f7d589a371bea93c64c92ef537e2870f436871a2
boyska <piuttosto@logorroici.org>
2014-11-09 14:21:26 +0100

    activate now includes virtualenvs

--- build-dep.sh
+++ build-dep.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-base=$(dirname $0)
-ve="$base/dep/ve"
+base=$(realpath $(dirname $0))
+ve="$base/dep/ve2"
+vethree="$base/dep/ve3"
 py="${ve}/bin/python"
+pythree="${vethree}/bin/python"
+pipthree="${vethree}/bin/pip"
 pip="${ve}/bin/pip"
 cd "$base"
 git submodule init
 git submodule update
 virtualenv2 --no-site-packages "$ve"
-"${py}" dep/afew/setup.py install
+virtualenv3 --no-site-packages "$vethree"
+cd "dep/afew"
+"${pythree}" setup.py install
+cd "$base"
 "${pip}" install -r src/requirements.txt
 
