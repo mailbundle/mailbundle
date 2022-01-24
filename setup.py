231ca4d1a3393b04b421fd056db73baa034c5225
Blallo <blallo@autistici.org>
2022-01-24 11:24:24 +0100

    Great file reorg in package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import typing as T
 
 
 def get_dependencies() -> T.List[T.Text]:
-    dependencies:  T.List[T.Text] = []
+    dependencies: T.List[T.Text] = []
 
     with open("requirements.txt") as req:
         for line in req.readlines():
@@ -25,14 +25,22 @@ def get_version() -> T.Text:
 
     p = subprocess.run(
         ["git", "describe", "--match", '"v[0-9]*.[0-9]*.[0-9]*"', "HEAD"],
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
-    if p.returncode == 0:
+    if p.returncode == 0 and p.stdout:
         return p.stdout.decode("ASCII")[1:]
 
-    p = subprocess.check_output(["git", "rev-list", "-1", "HEAD"])
-    commit = p.decode("ASCII").strip("\n")
-    return f"0.0.0+{commit}"
+    p = subprocess.run(
+        ["git", "rev-list", "-1", "HEAD"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    if p.returncode == 0 and p.stdout:
+        commit = p.stdout.decode("ASCII").strip("\n")
+        return f"0.0.0+{commit}"
+
+    return "0.0.0+dev"
 
 
 def get_readme_content() -> T.Text:
@@ -58,6 +66,12 @@ setup(
     url="https://github.com/boyska/mailbundle",
     license=LICENSE,
     install_requires=get_dependencies(),
-    packages=find_packages(exclude=['tests']),
+    extras_require={
+        "dev": [
+            "ipdb",
+        ],
+    },
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
 )
